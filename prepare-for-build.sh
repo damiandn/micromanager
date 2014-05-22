8c04845c015a66ceb85e4daed626ae8d76703fc3
Luke Stuyvenberg <stuyvenberg@wisc.edu>
2014-05-22 09:09:27 -0500

    Old build scripts: limited 64-bit support
    I'm not sure how useful it is at this stage, but there's probably not
    going to be much more work on these scripts; there are some new ones on
    the way.
    
    Signed-off-by: Luke Stuyvenberg <stuyvenberg@wisc.edu>

--- prepare-for-build.sh
+++ prepare-for-build.sh
@@ -1,10 +1,14 @@
-STABLE_FIJI_URL=http://jenkins.imagej.net/job/Stable-Fiji
-FIJI_URL=$STABLE_FIJI_URL/lastSuccessfulBuild/artifact/fiji-win32.tar.gz
-JDK_URL="http://fiji.sc/cgi-bin/gitweb.cgi?p=java/win32.git;a=snapshot;h=HEAD;sf=tgz"
+plat="$1"
+winplat="win$(echo $plat | tail -c3)"
+echo $winplat
+
+STABLE_FIJI_URL="http://jenkins.imagej.net/job/Stable-Fiji"
+FIJI_URL="$STABLE_FIJI_URL/lastSuccessfulBuild/artifact/fiji-$winplat.tar.gz"
+JDK_URL="http://fiji.sc/cgi-bin/gitweb.cgi?p=java/$winplat.git;a=snapshot;h=HEAD;sf=tgz"
 
 WD="$(pwd)"
 SRC="$(cd ../ && pwd)"
-FIJI_JAVA_HOME="$WD/bin_Win32/java/win32/jdk1.6.0_24"
+FIJI_JAVA_HOME="$WD/bin_$plat/java/$plat/jdk1.6.0_24"
 
 (
 	cd $SRC &&
@@ -19,7 +23,7 @@ FIJI_JAVA_HOME="$WD/bin_Win32/java/win32/jdk1.6.0_24"
 				contrib@fiji.sc:/srv/git/mmanager-3rdparty
 		)
 	else
-		(cd 3rdpartypublic && git pull)
+		echo "(should pull; ignoring!)" #(cd 3rdpartypublic && git pull)
 	fi &&
 
 	if ! test -f $FIJI_JAVA_HOME/include/jni.h
@@ -35,17 +39,18 @@ FIJI_JAVA_HOME="$WD/bin_Win32/java/win32/jdk1.6.0_24"
 
 	(
 		cd $WD &&
-		if ! test -f bin_Win32/ImageJ-win32.exe
+		if ! test -f "bin_$plat/ImageJ-$plat.exe"
 		then
-			echo "Copying Fiji into Micro-Manager's bin_Win32/ directory" &&
+			echo "Copying Fiji into Micro-Manager's bin_$plat/ directory" &&
 			curl $FIJI_URL |
 			(
-				cd bin_Win32/ &&
+				cd "bin_$plat/" &&
 				tar --strip-components=1 -xzf - &&
-				./ImageJ-win32.exe --update add-update-site \
+				"./ImageJ-$winplat.exe" --update add-update-site \
 					OpenSPIM http://openspim.org/update/ \
 					spim@openspim.org update/
 			)
 		fi
 	)
 )
+
